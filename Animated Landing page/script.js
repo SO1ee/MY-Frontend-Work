c7466b4808286d0c61c95e047436eea324f44a4a
Your Name <you@example.com>
2023-07-07 19:51:37 +0530

    First commit

---
+++ Animated Landing page/script.js
@@ -0,0 +1,15 @@
+gsap.to(".imagecontainer", {
+     width: "100%",
+      ease: "Sine.out",
+      duration: 6,
+      stagger: 6,
+      repeat:-1
+});
+gsap.to(".heading", {
+    top: "-100%",
+    ease: "Sine.out",
+    duration:6,
+    stagger: 6,
+    repeat:-1
+});
+  
